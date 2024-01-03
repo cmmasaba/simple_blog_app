d6f3fe7401e0f33f32f2e2edce885876ccdcb07f
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-01-03 15:40:40 +0300

    Added view for individual blog pages

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,7 @@ class BlogListView(ListView):
     """class view for the blog page"""
     model = Post
     template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
