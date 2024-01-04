00fbcaa10ef91eab785a62c9c8c9a9f9405c6528
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-01-04 15:53:28 +0300

    Added url pattern for delete page

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.views.generic.edit import (
     UpdateView,
     DeleteView
     )
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -30,4 +31,5 @@ class BlogUpdateView(UpdateView):
 class BlogDeleteView(DeleteView):
     model = Post
     template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
 
