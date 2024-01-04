18684bfa4543c44d7126cf52d559dacebe52d34c
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-01-04 15:52:07 +0300

    Added url pattern for delete page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView
+    )
 
 from .models import Post
 
@@ -22,4 +26,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
 
